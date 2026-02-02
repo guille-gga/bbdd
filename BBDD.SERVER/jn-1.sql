f474ba4e89bc1c06fd240491a79d08f8ca340605
Guillermo García Alguacil <guillegarci07@gmail.com>
2026-02-02 22:40:09 +0100

    Fix SQL queries and table name references
    Updated table names and corrected SQL syntax.

--- BBDD.SERVER/jn-1.sql
+++ BBDD.SERVER/jn-1.sql
@@ -4,13 +4,13 @@ libros.titulo AS libro,
 generos.nombre AS genero
 usuarios.nombre AS libro, 
 libros.titulo AS libro, 
-reseñas.puntuacion AS libro, 
+resenas.puntuacion AS libro, 
 
 FROM libros
 JOIN libros_autores ON libros.id = libros_autores.libro_id
 JOIN autores ON libros_autores.autores_id = autor.id
 JOIN libros_generos ON libro_id
 
-FROM reseñas
-JOIN usuarios ON reseñas.usuario_id = usuarios.id
-JOIN libros ON reseñas.libro_id = libros.id;
+FROM resenas
+JOIN usuarios ON resenas.usuario_id = usuarios.id
+JOIN libros ON resenas.libro_id = libros.id;
